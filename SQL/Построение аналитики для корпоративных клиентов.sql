52659ffa8d5c728cce67eefb5b644ecbe96f530b
Юлия <yayaskina@gmail.com>
2025-08-21 21:40:29 +0700

    Update Построение аналитики для корпоративных клиентов.sql

--- SQL/Построение аналитики для корпоративных клиентов.sql
+++ SQL/Построение аналитики для корпоративных клиентов.sql
@@ -6,3 +6,20 @@ FROM
 WHERE
 	company_id = 1
 [[and date_joined between {{date1}} and {{date2}}]]
+
+-- DAU - количество уникальных пользователей
+SELECT
+	date(entry_at) as ymd,
+	count(distinct user_id) as DAU
+FROM
+	userentry
+left join users 
+on
+	userentry.user_id = users.id
+left join company 
+on
+	users.company_id = company.id
+WHERE
+	company_id = 1
+ [[and entry_at between {{date1}} and {{date2}}]]
+group by 1
